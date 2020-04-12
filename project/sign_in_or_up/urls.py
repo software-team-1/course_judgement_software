b61aa34de5821f2135e939aaca0a53a068cbb105
mo666666 <760960081@qq.com>
2020-04-12 19:38:57 +0800

    try to build home page

---
+++ project/sign_in_or_up/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+urlpatterns = [
+    path('', home)
+]
