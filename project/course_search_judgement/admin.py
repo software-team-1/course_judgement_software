e61a890d5a2d140e1d2c514a43d5b249c2d1242b
mo666666 <760960081@qq.com>
2020-04-20 13:23:51 +0800

    the database is successfully developed

--- project/course_search_judgement/admin.py
+++ project/course_search_judgement/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib import admin
 
+from .models import course,judgement_system
+
+admin.site.register(course)
+admin.site.register(judgement_system)
 # Register your models here.
--- project/course_search_judgement/models.py
+++ project/course_search_judgement/models.py
@@ -1,6 +1,18 @@
 # Create your models here.
+from django.db import models
+from log_sign.models import normal_user
+import datetime
+
+
 class course(models.Model):
     # 课程名
     name = models.CharField(max_length=20, null=False)
     # 授课教师
     teacher = models.CharField(max_length=10, null=False)
+
+
+class judgement_system(models.Model):
+    name = models.ForeignKey(normal_user, on_delete=models.CASCADE)
+    course = models.ForeignKey(course, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(default=datetime.datetime.now())
+    evaluation = models.CharField(max_length=500, null=False)
