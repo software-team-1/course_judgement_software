76ae989cab17dcad8357e24f3adc70af43546be3
Lin Bian <1132090987@qq.com>
2020-04-20 12:01:50 +0800

    create models

--- project/course_search_judgement/models.py
+++ project/course_search_judgement/models.py
@@ -1,1 +1,6 @@
-
+# Create your models here.
+class course(models.Model):
+    # 课程名
+    name = models.CharField(max_length=20, null=False)
+    # 授课教师
+    teacher = models.CharField(max_length=10, null=False)
