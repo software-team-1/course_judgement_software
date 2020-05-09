3bcc0783aa510d13d07aec1c5d289ca7ff8e3e57
shjdzh <shjdzh@sjtu.edu.cn>
2020-05-09 20:46:30 +0800

    change

--- project/my_account/urls.py
+++ project/my_account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 app_name = 'my_account'
 urlpatterns = [
-    path('', views.my_account),
+    path('', views.my_account, name='my_account'),
     path('modify_judgement/', views.modify_judgement),
     path('delete_judgement/', views.delete)
 
