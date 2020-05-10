76e1f2cc7fac390f16dbc96d5fbdb767d2075a61
mo666666 <760960081@qq.com>
2020-05-10 10:11:52 +0800

    add the function of judgement for course

--- project/course_search_judgement/urls.py
+++ project/course_search_judgement/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('search_from/', views.search_from, name='search_from'),
     path('search_result/', views.search_result, name='search_result'),  #结果查询显示页面
     path('<int:p1>/', views.judgement),
-    path('add_judgement/', views.add_judgement)  # 提交课程的评价结果
+    path('add_judgement/', views.judgement_for_course),  # 提交课程的评价结果
+    path('add_judgement/submit', views.add_judgement)  # 提交课程的评价结果
 ]
--- project/course_search_judgement/views.py
+++ project/course_search_judgement/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import course, judgement_system
+from log_sign.models import normal_user
+from datetime import datetime
 
 
 # Create your views here.
@@ -43,18 +45,37 @@ def judgement(request, p1):
     return render(request, 'judgement.html', {'judgement': judges})
 
 
+def judgement_for_course(request):
+    return render(request, 'add_judgement.html')
+
+
 def add_judgement(request):
-    info = request.params['data']
+    if request.method == "POST":
+        comment_of_the_course = request.POST.get("comment_of_the_course")
+        comment_of_the_teacher = request.POST.get("comment_of_the_teacher")
+        the_constitution_of_grade = request.POST.get("the_constitution_of_grade ")
+        time_for_homework = request.POST.get("time_for_homework")
+        hard_core_mark = request.POST.get("hard_core_mark")
+        recommend_mark = request.POST.get("recommend_mark")
+        mark_of_interest = request.POST.get("mark_of_interest")
+        course_id = request.POST.get("course_id")
 
-    # 从请求消息中 获取要添加评论的信息
-    # 并且插入到数据库中
-    # 返回值 就是插入使得否正确
-    record = judgement_system.objects.create(name=info['name'],
-                                             course=['course'],
-                                             mark=info['mark'],
-                                             time_for_homework=info['time_for_homework'],
-                                             the_constitution_of_grade=info['the_constitution_of_grade'],
-                                             comment_of_the_course=info['comment_of_the_course'],
-                                             comment_of_the_teacher=info['comment_of_the_teacher'])
+        # judge = request.POST.get("id")
+        # modify_judge = judgement_system.objects.filter(id=judge)
+        # course_id = modify_judge[0].course_id
+        # modify_judge.delete()
+        user = request.user
+        user_ = normal_user.objects.filter(user=user)
+        user_id = user_[0].id
+        judgement_system(create_date=datetime.now(),
+                         comment_of_the_course=comment_of_the_course,
+                         comment_of_the_teacher=comment_of_the_teacher,
+                         the_constitution_of_grade=the_constitution_of_grade,
+                         time_for_homework=time_for_homework,
+                         course_id=course_id,
+                         hard_core_mark=hard_core_mark,
+                         recommend_mark=recommend_mark,
+                         mark_of_interest=mark_of_interest,
+                         name_id=user_id).save()
 
-    return render(request, {'ret': 0})  # 注意，还没有添加HTML文件
+    return redirect("/search_result/")  # 注意，还没有添加HTML文件
